a4c42051bf1781f8127797037924e07e4f0c62b4
Shreesha99 <shreeshavenkatram99@gmail.com>
2026-03-06 15:39:45 +0530

    test task (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
 
 export default function Home() {
   return (
-    <p className="font-bold text-green-500">
-      <Button>
-        hello button
-      </Button>
-    </p>
+      <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-sky-200 to-red-500">
+        <div className="space-y-6 text-center">
+          <h1 className={cn(
+            "text-6xl font-semibold text-white  drop-shadow-md",
+            font.className
+            )}>
+          🔐 Auth
+          </h1>
+          <p className="text-white text-lg">
+            A simple authentication package
+          </p>
+        </div>
+        <div>
+          <LoginButton>
+          <Button variant="secondary" size="lg" className="mt-3">
+            Sign in
+          </Button>
+          </LoginButton>
+        </div>
+      </main>
   );
 }
